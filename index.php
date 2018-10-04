f704c0626b7c89c25105d0c00f51dcfd1bc4d7f0
signupromain <romain@machaharson.com>
2018-10-04 16:23:47 +0200

    gallery

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <link href="https://fonts.googleapis.com/css?family=Oswald:700" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ENTRAIDE & CULTURE</title>
     <link rel="stylesheet" href="css/style.css">
     <script type='text/javascript' href='js/script.js'></script>
--- presentation.php
+++ presentation.php
@@ -74,7 +74,42 @@
     <div id="conteneur">
         <div class=column_A>
             <div class=zoom>
-                <div class="col-lg-4 col-md-4 col-sm-4 col-xs-6 col-ss-12 padbot30 gallery_img lorem"><a href="images/gallery/child.jpg" data-gal="prettyPhoto[gal]"><i></i><img src="images/gallery/child.jpg" alt="" style="width: 100%;" /></a></div>
+                <div class="col-lg-1 padbot30 gallery_img lorem" id="grandephoto">
+                    <a href="images/gallery/child_512.jpg" data-gal="prettyPhoto[gal]">
+                        <i></i>
+                        <img src="images/gallery/child_512.jpg" alt="" style="width: 100%;" /></a>
+                </div>
+            </div>
+            <script type="text/javascript">
+                function afficheGrandePhoto(imageCliquee) {
+                    /* je récupère dans l'attribut alt de l'image cliquée son nom */
+                    console.log("afficheGrandePhoto : " + imageCliquee.alt);
+                    /* URL de la vignette cliquée ? */
+                    var URLvignette = imageCliquee.getAttribute("src");
+                    console.log("URL de la photo cliquée : " + URLvignette);
+                    /* Enlever dans l'URL de ma vignette le "128" et le remplacer par "512" pour avoir l'URL de la grande photo correspondante */
+                    var URLgrandephoto = URLvignette.substr(0,(URLvignette.length - 7)) + "512.jpg";
+                    console.log(URLgrandephoto);
+                    /* Modifier l'attribut src de la grande photo */
+                    var grandePhoto = document.querySelector("#grandephoto img");
+                    grandePhoto.setAttribute("src", URLgrandephoto);
+                    grandePhoto.setAttribute("alt", imageCliquee.alt);
+                }
+            </script>
+            <div id="global">
+                <section>
+
+
+                </section>
+                <section id="vignettes">
+                    <ul>
+                        <li><img src="images/gallery/child_128.jpg" alt="" width="128" height="96" onclick="afficheGrandePhoto(this);" /></li>
+                        <li><img src="images/gallery/hands_128.jpg" alt="" width="128" height="96" onclick="afficheGrandePhoto(this);"/></li>
+                        <li><img src="images/gallery/bike_128.jpg" alt="" width="128" height="96" onclick="afficheGrandePhoto(this);" /></li>
+                        <li><img src="images/gallery/friends_128.jpg" alt="" width="128" height="96" onclick="afficheGrandePhoto(this);" /></li>
+                        <li><img src="images/gallery/ok_128.jpg" alt="" width="128" height="96" onclick="afficheGrandePhoto(this);" /></li>
+                    </ul>
+                </section>
             </div>
             <div class="imagePara">
                 <p>L’ASBL Entraide et Culture, créée en 1983 à Forest par des bénévoles, apporte une aide et un soutien à un public précarisé et fragilisé.</p>
