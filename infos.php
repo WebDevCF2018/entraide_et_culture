2df81cadf2d6e80131cc83f024c03ba9bb076401
signupromain <romain@machaharson.com>
2018-10-05 13:42:23 +0200

    infos

--- infos.php
+++ infos.php
@@ -72,102 +72,112 @@
     </div>
     <!---------------conteneur infos pratiques------------------->
     <div id="conteneur">
-        <div id="myDIV2"></div>
-        <section id="contacts">
-            <!-- Container contacts -->
-            <div class="container">
-                <div class="page_title center">
-                </div>
-                <!-- Row -->
-                <div class="row">
+            <div class=column_A_accueil>
+                <div class="rowmap">
                     <div class="col-lg-4 col-md-4 padbot30">
                         <div id="map">
                             <iframe height="300" style="border:none; scrolling:no; marginheight:0; marginwidth:0;" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d26448.534201747054!2d-118.24590761238238!3d34.042158170531856!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80c2c75ddc27da13%3A0xe22fdf6f254608f4!2sLos+Angeles%2C+California%2C+Stati+Uniti!5e0!3m2!1sit!2sit!4v1422286433932">
                             </iframe>
                         </div>
                     </div>
-                    <div class="col-lg-4 col-md-4 padbot30 our_office_wrap">
-                        <h6>Our Location</h6>
-                        <p>79., Grigum street, Suite 041 San Diego, DI 003410</p>
-                        <p>Email: info@yourdomain.com</p>
-                        <p>Phone: +1 (318) 420 3456</p>
-                        <p>Fax: +1 (318) 420 3456</p>
-                        <h6>Get Social</h6>
-                        <div class="social">
-                            <a href="javascript:void(0);"><i class="fa fa-vimeo-square"></i></a>
-                            <a href="javascript:void(0);"><i class="fa fa-twitter"></i></a>
-                            <a href="javascript:void(0);"><i class="fa fa-tumblr"></i></a>
-                            <a href="javascript:void(0);"><i class="fa fa-linkedin"></i></a>
+                </div>
+
+        <div class=column_A>
+            <div class="col-md-6 padbot30">
+                <div class="clients_description">
+                    <div class="blocPartenaires">
+                        <div class="titlePartenaires">nos partenaires</div>
+                        <div class="titrePartenaires">
+                            <h3>Title</h3>
+                        </div>
+                        <br>
+                        <div class="textePartenaires">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur a neque a metus congue congue at sed metus. Aliquam quis augue dictum.</p>
                         </div>
                     </div>
+                </div>
+            </div>
+        </div>   </div>
+            <div class=column_B_accueil style="overflow: auto;
+                 height: 100px;">
+
+                <div id="myDIVentraide"></div>
+                <div class="mainTexte">
+                    <p>ASBL FONDÉE EN 1983 APPORTE UNE AIDE ET </br> UN SOUTIEN À UN PUBLIC PRÉCARISÉ ET FRAGILISÉ.</p>
+                </div>
+                <div class="formeinfo1">
                     <div class="col-lg-4 col-md-4 padbot30 our_office_wrap">
                         <div id="contact-form">
-                            <div id="sendmessage">Your message has been sent. Thank you!</div>
+                            <div id="sendmessage">Votre message a été bien envoyé. Merci!</div>
                             <form class="contactForm" action="contact/contact.php" method="post" >
                                 <div class="form-group">
-                                    <input id="name" type="text" name="name" placeholder="Name" data-rule="maxlen:4" data-msg="Please enter at least 4 chars" />
+                                    <input id="name" type="text" name="name" placeholder="Nom" data-rule="maxlen:4" data-msg="SVP, entrer minimun 4 charactères" />
                                     <div class="validation"></div>
                                 </div>
                                 <div class="form-group">
-                                    <input id="email" type="email" name="email" placeholder="Email" data-rule="email" data-msg="Please enter a valid email" />
+                                    <input id="email" type="email" name="email" placeholder="E-mail" data-rule="email" data-msg="Votre e-mail" />
                                     <div class="validation"></div>
                                 </div>
                                 <div class="form-group">
-                                    <textarea name="message" placeholder="Message" data-rule="required" data-msg="Please enter your message"></textarea>
+                                    <textarea name="message" placeholder="Message" data-rule="required" data-msg="SVP, écrire votre message"></textarea>
                                     <div class="validation"></div>
                                 </div>
-                                <button type="submit">Send Message</button>
+                                <button type="submit">Envoyer message</button>
                             </form>
                         </div>
                     </div>
                 </div>
-                <!-- //Row -->
-            </div>
-            <!-- //Container -->
-        </section>
-        <section id="clients">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-6 padbot30">
-                        <div class="clients_description">
-                            <div class="blocPartenaires">
-                                <div class="titlePartenaires">nos partenaires</div>
-                                <div class="titrePartenaires">
-                                    <h3>Title</h3>
-                                </div>
-                                <br>
-                                <div class="textePartenaires">
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur a neque a metus congue congue at sed metus. Aliquam quis augue dictum, tristique purus in, tempor nisi. Nam congue volutpat aliquet. Pellentesque laoreet ut purus vehicula laoreet. Curabitur tincidunt justo tortor, ut vulputate velit volutpat laoreet. Quisque fringilla, velit vitae tincidunt dignissim, nulla dui accumsan lorem.</p>
+                <div class="formeinfo2">
+                    <section id="contacts">
+                        <!-- Container contacts -->
+                        <div class="adresse">
+                            <div class="page_title center">
+                            </div>
+                            <!-- Row -->
+
+                            <div class="col-md-4 padbot30 our_office_wrap">
+                                <h6>Notre location</h6>
+                                <p>Avenue Van Volxem,<br>525 à 1190 Forest</p>
+                                <p>E-mail : secretariat@entraideetculture.be</p>
+                                <p>Tél. : 02 539 43 60</p>
+                                <p>Fax : 02 539 43 70</p>
+                                <h6>Réseau social</h6>
+                                <div class="social">
+                                    <a href="javascript:void(0);"><i class="fa fa-facebook"></i></a>
                                 </div>
                             </div>
+
                         </div>
+                        <!-- //Row -->
+                </div>
+        <div class=column_B>
+            <div class="col-md-6 padbot30">
+                <div class="row clients_wrap">
+                    <div class="col-lg-6 col-md-6 col-sm-4 col-xs-6 padbot10">
+                        <a class="client_logo" href="javascript:void(0);"><img src="images/clients/6.png" alt="" /></a>
                     </div>
-                    <div class="col-md-6 padbot30">
-                        <div class="row clients_wrap">
-                            <div class="col-lg-6 col-md-6 col-sm-4 col-xs-6 padbot10">
-                                <a class="client_logo" href="javascript:void(0);"><img src="images/clients/6.png" alt="" /></a>
-                            </div>
-                            <div class="col-lg-6 col-md-6 col-sm-4 col-xs-6 padbot10">
-                                <a class="client_logo" href="javascript:void(0);"><img src="images/clients/6.png" alt="" /></a>
-                            </div>
-                            <div class="col-lg-6 col-md-6 col-sm-4 col-xs-6 padbot10">
-                                <a class="client_logo" href="javascript:void(0);"><img src="images/clients/6.png" alt="" /></a>
-                            </div>
-                            <div class="col-lg-6 col-md-6 col-sm-4 col-xs-6 padbot10">
-                                <a class="client_logo" href="javascript:void(0);"><img src="images/clients/6.png" alt="" /></a>
-                            </div>
-                            <div class="col-lg-6 col-md-6 col-sm-4 col-xs-6 padbot10">
-                                <a class="client_logo" href="javascript:void(0);"><img src="images/clients/6.png" alt="" /></a>
-                            </div>
-                            <div class="col-lg-6 col-md-6 col-sm-4 col-xs-6 padbot10">
-                                <a class="client_logo" href="javascript:void(0);"><img src="images/clients/6.png" alt="" /></a>
-                            </div>
-                        </div>
+                    <div class="col-lg-6 col-md-6 col-sm-4 col-xs-6 padbot10">
+                        <a class="client_logo" href="javascript:void(0);"><img src="images/clients/6.png" alt="" /></a>
+                    </div>
+                    <div class="col-lg-6 col-md-6 col-sm-4 col-xs-6 padbot10">
+                        <a class="client_logo" href="javascript:void(0);"><img src="images/clients/6.png" alt="" /></a>
+                    </div>
+                    <div class="col-lg-6 col-md-6 col-sm-4 col-xs-6 padbot10">
+                        <a class="client_logo" href="javascript:void(0);"><img src="images/clients/6.png" alt="" /></a>
+                    </div>
+                    <div class="col-lg-6 col-md-6 col-sm-4 col-xs-6 padbot10">
+                        <a class="client_logo" href="javascript:void(0);"><img src="images/clients/6.png" alt="" /></a>
+                    </div>
+                    <div class="col-lg-6 col-md-6 col-sm-4 col-xs-6 padbot10">
+                        <a class="client_logo" href="javascript:void(0);"><img src="images/clients/6.png" alt="" /></a>
                     </div>
                 </div>
             </div>
-        </section>
-    </div>
+                </div>
+            </div>
+
+
     <!---------------END conteneur infos pratiques------------------->
+
 </div>
 </div>
